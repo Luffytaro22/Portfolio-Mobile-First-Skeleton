4186b950848740b56fd7367bfbc8f26955fc0f1b
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-16 10:43:24 -0500

    Saved project's information in objects

---
+++ projects.js
@@ -0,0 +1,34 @@
+let projects = [
+  { 
+    name: "Calculator",
+    description: "A simple calculator that can do the basics operations.",
+    image: "Images\calculator.png",
+    languages: ["HTML", "CSS", "Javascript"],
+    liveLink: "https://luffytaro22.github.io/Calculator/",
+    sourceLink: "https://github.com/Luffytaro22/Calculator"
+  },
+  {
+    name: "Etch-A-Aketch",
+    description: "A recreation of the Etch-A-Sketch interactive dashboard where you can select the quantity of spaces and the colors.",
+    image: "Images\etch-a-sketch.png",
+    languages: ["HTML", "CSS", "Javascript"],
+    liveLink: "https://luffytaro22.github.io/Etch-A-Sketch/",
+    sourceLink: "https://github.com/Luffytaro22/Etch-A-Sketch"
+  },
+  {
+    name: "Rock-Paper-Scissors",
+    description: "The global-popular game against the computer. Will you be winner or the losser? Try it out!",
+    image: "Images\rock-paper-scissors.png",
+    languages: ["HTML", "CSS", "Javascript"],
+    liveLink: "https://luffytaro22.github.io/Rock-Paper-Scissors/",
+    sourceLink: "https://github.com/Luffytaro22/Rock-Paper-Scissors"
+  },
+  {
+    name: "Form Page",
+    decription: "A form page that collects user info with a nice-looking design",
+    image: "Images\form-page.png",
+    languages: ["HTML", "CSS"],
+    liveLink: "https://luffytaro22.github.io/Sign-Up_form/",
+    sourceLink: "https://github.com/Luffytaro22/Sign-Up_form"
+  }
+];
