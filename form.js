660b573ceca136a932691146a4bd7929dfca1fdd
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-17 12:00:52 -0500

    Added the span and user elements to the form control

--- form.js
+++ form.js
@@ -1,17 +1,29 @@
 const form = document.querySelector('form');
 const email = document.querySelector('input[type="email"]');
-const span = document.querySelector('.error');
+const user = document.querySelector('input[type="text"]')
+const spanMail = document.querySelector('#mail');
+const spanUser = document.querySelector('#user');
 
-function showError() {
+/* A function for the email's errors */
+function showErrorMail() {
   /* If the email input is empty */
   if (email.validity.valueMissing) {
-    span.textContent = 'Please enter a value.';
-  } else if (email.validity.typeMismatch) {
-    /* If the input is not an email type */
-    span.textContent = 'Please enter a valid email.';
+    spanMail.textContent = '* Please enter a value.';
   } else if (email.value !== email.value.toLowerCase()) {
     /* If the input is not in lower case */
-    span.textContent = 'Only enter characters in lower case.'
+    spanMail.textContent = '* Only enter characters in lower case.'
+  } else if (email.validity.typeMismatch) {
+    /* If the input is not an email type */
+    spanMail.textContent = '* Please enter a valid email.';
+  }
+}
+
+/* A function for the User's name errors */
+function showErrorUser() {
+  if (user.validity.valueMissing) {
+    spanUser.textContent = '* Please enter a value.';
+  } else if (user.validity.typeMismatch) {
+    spanUser.textContent = '* Please enter a valid name.';
   }
 }
 
@@ -19,19 +31,32 @@ function showError() {
 email.addEventListener('input', () => {
   if (email.validity.valid) {
     /* If it's valid the text of the error content dissapears */
-    span.textContent = '';
-    span.className = 'error';
+    spanMail.textContent = '';
+    spanMail.className = 'error';
   } else {
     /* If not, then displays the errors */
-    showError();
+    showErrorMail();
+  }
+});
+
+/* A input listener to the user input */
+user.addEventListener('input', () => {
+  if (user.validity.valid) {
+    spanUser.textContent = '';
+    spanUser.className = 'error';
+  } else {
+    showErrorUser();
   }
 });
 
 /* A submit listener to the form element */
 form.addEventListener('submit', (event) => {
-  if (!email.validity.valid) {
+  if (!user.validity.valid) {
+    showErrorUser();
+    event.preventDefault();
+  } else if (!email.validity.valid) {
     /* If it's not valid, displays the error and avoids the form to be submmitted. */
-    showError();
+    showErrorMail();
     event.preventDefault();
   }
 })
