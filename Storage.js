c22774c88a123fbf6212d7ebd9099b71a7799e73
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-18 12:31:23 -0500

    Added the information of the inputs to the local Storage

--- Storage.js
+++ Storage.js
@@ -1,15 +1,22 @@
 /* Referencing the name and email input values */
-const name = document.querySelector('input[name="name"]');
-const email = document.querySelector('input[name="email"]');
-let nameValue = document.querySelector('input[name="name"]').value;
-let emailValue = document.querySelector('input[name="email"]').value;
+const inputs = document.querySelectorAll('input');
 
 /* Creating an object for the input's values */
 let input = {
-  'name': nameValue,
-  'email': emailValue,
+  'name': '',
+  'email': '',
 };
 
-/* Adding the input's information to the localStorage object */
-localStorage.setItem('form', JSON.stringify(input));
+function changeInput() {
+  /* Changing the input's keys every time the inputs fields change */
+  input.name = document.querySelector('input[name="name"]').value;
+  input.email = document.querySelector('input[name="email"]').value;
+
+  /* Adding the input's information to the localStorage object */
+  localStorage.setItem('form', JSON.stringify(input));
+}
+
+/* An input listener to each input (name and email) */
+inputs.forEach((input) => input.addEventListener('input', changeInput));
+
 
